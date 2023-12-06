dbb56cfa672f871bc3d04759d64c90f0ac1b3628
itsnimadaris <nimarj43@gmail.com>
2023-12-06 17:52:26 +0330

    adding lazyLoad , configure and children

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,26 +1,86 @@
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-//pages
-import Home from "../pages/Home";
-import Admin from "../pages/Admin";
-import Cart from "../pages/Cart";
+
+const Home = lazy(() => import("../pages/Home"));
+const Admin = lazy(() => import("../pages/Admin"));
+const Cart = lazy(() => import("../pages/Cart"));
+const FinalizationOrder = lazy(() => import("../pages/FinalizationOrder"));
+const Payment = lazy(() => import("../pages/Payment"));
+const Layout = lazy(() => import("../layouts/Layout"));
+const AdministrationPanel = lazy(() => import("../pages/AdministrationPanel"));
+
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <header>
         <nav>
           <h1>
-            Jobarouter
             <Link to="/">Home</Link>
-            <NavLink to="Admin">Admin</NavLink>
-            <NavLink to="Cart">Cart</NavLink>
+            <NavLink to="/admin">Admin</NavLink>
+            <NavLink to="/cart">Cart</NavLink>
           </h1>
         </nav>
       </header>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="Admin" element={<Admin />} />
-          <Route path="Cart" element={<Cart/>} />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Layout />
+              </Suspense>
+            }
+          >
+            <Route
+              index
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Home />
+                </Suspense>
+              }
+            />
+            <Route
+              path="admin"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Admin />
+                </Suspense>
+              }
+            />
+            <Route
+              path="cart"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Cart />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/cart/finalizationOrder/"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <FinalizationOrder />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/cart/finalizationOrder/payment"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Payment />
+                </Suspense>
+              }
+            />
+            <Route
+              path="admin/AdministrationPanel"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <AdministrationPanel />
+                </Suspense>
+              }
+            />
+          </Route>
+          
         </Routes>
       </main>
     </BrowserRouter>
