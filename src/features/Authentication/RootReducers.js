3ecc5ab6f8434dd3ec49916faff3a8c2e38f6a91
itsnimadaris <nimarj43@gmail.com>
2023-12-26 13:21:19 +0330

    Added rootReducers.js

---
+++ src/features/Authentication/RootReducers.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./auth/authSlice";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+export default rootReducer;
