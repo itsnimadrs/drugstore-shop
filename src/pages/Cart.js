c49774a6f996d02e0120b445c1b83b9085dd0c37
itsnimadaris <nimarj43@gmail.com>
2024-01-03 15:50:02 +0330

    changed the ui of cart and orders and payment

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,15 +1,66 @@
-import { Link, Outlet } from "react-router-dom";
+"use client";
 
-export default function Cart() {
+import { Table } from "flowbite-react";
+import { Link, Outlet } from "react-router-dom";
+import { createContext, useState, useEffect } from "react";
+import { Button } from "flowbite-react";
+
+export default function Cart({ children }) {
   return (
     <div>
+      <div className="font-bold text-3xl flex justify-end m-auto mx-10 my-10">
+        سبد خرید
+      </div>
+      <div className="overflow-x-auto">
+        <Table>
+          <Table.Head>
+            <Table.HeadCell>Product name</Table.HeadCell>
+            <Table.HeadCell>Color</Table.HeadCell>
+            <Table.HeadCell>Category</Table.HeadCell>
+            <Table.HeadCell>Price</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Edit</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell>
+                <a
+                  href="#"
+                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  حذف از سبد خرید
+                </a>
+              </Table.Cell>
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                Magic Mouse 2
+              </Table.Cell>
+              <Table.Cell>Black</Table.Cell>
+              <Table.Cell>Accessories</Table.Cell>
+              <Table.Cell>$99</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </div>
       <div>
         <Outlet />
-        <Link to="finalizationOrder"> نهایی کردن سبد خرید</Link> <br />
-        <Link to="/"> بازگشت به سایت</Link>
-
+        <Link to="finalizationOrder">
+          <Button
+            className="flex justify-center align-middle m-auto my-10 w-60"
+            color="success"
+          >
+            نهایی کردن سبد خرید
+          </Button>
+        </Link>{" "}
+        <br />
+        <Link
+          className="flex justify-center m-auto text-justify underline text-lg font-normal"
+          to="/"
+        >
+          {" "}
+          بازگشت به سایت
+        </Link>
       </div>
-      Cart
     </div>
   );
 }
--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,5 +1,37 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import payment from "../assets/payment.png";
+
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+import Success from "./success";
 export default function Payment() {
-  return <div>Payment</div>;
+  return (
+    <>
+      <div>
+        <img src={payment} />
+      </div>
+      <div>
+        <Link to="/cart/finalizationOrder/payment/success">
+          <Button
+            className="w-[22rem] rounded-2xl absolute bottom-10 right-[23rem]"
+            color="success"
+          >
+            پرداخت
+          </Button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/cart/finalizationOrder/payment/notsuccess">
+          <Button
+            className="w-[8rem] rounded-2xl absolute bottom-10 right-[46rem]"
+            color="warning"
+          >
+            انصراف
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
 }
